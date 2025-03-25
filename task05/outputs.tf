1f9c13044946d88b56b3233523a5cb273abd930b
shashank <shashankbandari777@gmail.com>
2025-03-25 16:48:12 +0530

    today

--- task05/outputs.tf
+++ task05/outputs.tf
@@ -3,16 +3,16 @@ output "traffic_manager_fqdn" {
   description = "The fully qualified domain name of the Traffic Manager profile"
 }
 
-output "resource_group_names" {
-  value = {
-    rg1 = module.rg1.name,
-    rg2 = module.rg2.name,
-    rg3 = module.rg3.name
-  }
-  description = "Resource group names and their locations"
-}
+# output "resource_group_names" {
+#   value = {
+#     rg1 = module.rg1.name,
+#     rg2 = module.rg2.name,
+#     rg3 = module.rg3.name
+#   }
+#   description = "Resource group names and their locations"
+# }
 
-output "app_service_urls" {
-  value       = [module.app1.default_hostname, module.app2.default_hostname]
-  description = "The URLs of the deployed App Services"
-}
+# output "app_service_urls" {
+#   value       = [module.app1.default_hostname, module.app2.default_hostname]
+#   description = "The URLs of the deployed App Services"
+# }
