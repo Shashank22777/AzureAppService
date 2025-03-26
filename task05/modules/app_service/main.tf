937e1e05edef66210d4274ef84370c8313719b2a
shashank <shashankbandari777@gmail.com>
2025-03-26 18:20:30 +0530

    t10

--- task05/modules/app_service/main.tf
+++ task05/modules/app_service/main.tf
@@ -19,7 +19,7 @@ resource "azurerm_windows_web_app" "web_app" {
         ip_address  = try(ip_restriction.value.ip_address, null)  # IP address can be missing
         service_tag = try(ip_restriction.value.service_tag, null) # Service tag can be missing
         action      = ip_restriction.value.action
-        #priority    = ip_restriction.value.priority
+        priority    = ip_restriction.value.priority
       }
     }
   }
